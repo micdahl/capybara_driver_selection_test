79680ccfbc42ae5a56e35bf88316a8ebe2546486
Michael Dahl <michael.dahl84@gmail.com>
2021-01-06 12:42:40 +0100

    Select driver via driven_by

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
-
   it "does show page" do
     visit "home/index"
     expect(page).to have_content("Home#index")
